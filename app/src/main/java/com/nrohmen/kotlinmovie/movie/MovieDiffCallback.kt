5d5648dd3414c22613e7fcd2111060225daa3667
khrisna <khrisna.indrawan@gmail.com>
2019-10-29 04:05:03 +0700

    refactor list adapter from RecyclerView.Adapter to ListAdapter

---
+++ app/src/main/java/com/nrohmen/kotlinmovie/movie/MovieDiffCallback.kt
@@ -0,0 +1,16 @@
+package com.nrohmen.kotlinmovie.movie
+
+import android.support.v7.util.DiffUtil
+import com.nrohmen.kotlinmovie.models.Movie
+
+class MovieDiffCallback : DiffUtil.ItemCallback<Movie>() {
+
+    override fun areItemsTheSame(oldItem: Movie?, newItem: Movie?): Boolean {
+        return oldItem?.id == newItem?.id
+    }
+
+    override fun areContentsTheSame(oldItem: Movie?, newItem: Movie?): Boolean {
+        return oldItem == newItem
+    }
+
+}
